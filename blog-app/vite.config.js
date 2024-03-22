9549769bfae0e8053be52956d8c84aa98176e99d
devibifaa <ib2link@yahoo.com>
2024-03-22 01:54:06 +0100

    update

---
+++ blog-app/vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist', // Specify the output directory for the build process (default is 'dist')
+  },
+});
